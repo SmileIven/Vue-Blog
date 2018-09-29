a65a3939fc7a8401ba361267274fdc5a6076fb79
SmileIven <smileiven@163.com>
2018-09-29 17:40:45 +0800

    路由

--- src/routers/config.js
+++ src/routers/config.js
@@ -1,8 +1,11 @@
 import Index from '../components/Admin/Index.vue'
+import Welcome from '../components/Welcome.vue'
 
 export default {
+  mode: 'history',
   routes: [
-    { path: '*', component: Index }
-    
+    { path: '*', component: Welcome },
+    { path: '/admin', component: Index }
+
   ]
 }
