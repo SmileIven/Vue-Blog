b176283044962387c111883e8ffc26c18e360e19
SmileIven <smileiven@163.com>
2018-09-30 10:12:26 +0800

    添加页面

--- src/routers/config.js
+++ src/routers/config.js
@@ -1,11 +1,21 @@
-import Index from '../components/Admin/Index.vue'
+import Admin from '../components/Admin/Index.vue'
 import Welcome from '../components/Welcome.vue'
+import NotFound from '../components/NotFound.vue'
 
 export default {
   mode: 'history',
   routes: [
-    { path: '*', component: Welcome },
-    { path: '/admin', component: Index }
+    {
+      path: '/Admin',
+      component:Admin,
+      children: [
+        { path: '', component: Welcome },
+
+      ]
+    },
+    {
+      path:'*',component:NotFound,hidden:true
+    }
 
   ]
 }
